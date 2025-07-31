1bd1eb3e943e94b29d135c1a09419f1d949a22f0
FirePh0enix <leonard@delbecq.net>
2025-07-31 17:15:34 +0200

    Add pistol & bullet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,25 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _speed = 12.0f;
+    [SerializeField] private float _speed = 7.0f;
+    [SerializeField] private float _lookSpeedH = 3.0f;
+    [SerializeField] private float _lookSpeedV = 3.0f;
     [SerializeField] private Camera _camera;
+    [SerializeField] private Transform _head;
+    [SerializeField] private GameObject _bullet;
+    [SerializeField] private Transform _pistol;
+
     private Rigidbody _rb;
-    private PlayerInput _playerInput;
     private Vector2 _input;
+    private float _yaw = 0.0f;
+    private float _pitch = 0.0f;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
-        _playerInput = GetComponent<PlayerInput>();
     }
 
     private void Update()
@@ -32,8 +39,48 @@ public class PlayerController : MonoBehaviour
         _rb.linearVelocity = moveDirection.normalized * _speed + new Vector3(0, _rb.linearVelocity.y, 0);
     }
 
+    private void LateUpdate()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            _head.localEulerAngles = new Vector3(_pitch, _head.localEulerAngles.y, 0.0f);
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, _yaw, 0.0f);
+        }
+    }
+
     public void MoveInput(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
+    }
+
+    public void MouseInput(InputAction.CallbackContext context)
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Vector2 value = context.ReadValue<Vector2>();
+            _yaw += _lookSpeedH * value.x * 0.1f;
+            _pitch -= _lookSpeedV * value.y * 0.1f;
+        }
+    }
+
+    public void LockMouse(InputAction.CallbackContext context)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if (Cursor.lockState != CursorLockMode.Locked)
+        {
+            return;
+        }
+
+        GameObject bullet = Instantiate(_bullet, _pistol.transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody>().AddForce(_head.transform.forward * 8.0f, ForceMode.Force);
+    }
+
+    public void UnlockMouse(InputAction.CallbackContext context)
+    {
+        Cursor.lockState = CursorLockMode.None;
     }
 }
